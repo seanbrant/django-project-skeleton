756536417f3a5864969474beaffba7a515867460
Sean Brant <brant.sean@gmail.com>
2012-11-17 16:53:31 -0600

    Adds missing test settings base settings import

--- tests/settings.py
+++ tests/settings.py
@@ -1,3 +1,5 @@
+from {{ project_name }}.settings import *
+
 DEBUG = False
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 PASSWORD_HASHERS = ['django.contrib.auth.hashers.SHA1PasswordHasher']
